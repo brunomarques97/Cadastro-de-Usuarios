e6339a01499a2a00ea416d290672e256550eab63
brunomarques97 <116980642+brunomarques97@users.noreply.github.com>
2022-11-23 23:08:51 -0300

    Teste 2 de conexo com banco de dados

--- js/teste.js
+++ js/teste.js
@@ -3,6 +3,23 @@ const sequelize=new Sequelize('teste','root','123456',{
     host:"localhost",
     dialect:'mysql'
 })
+const postagem= sequelize.define('cadastro',{
+    nome:{
+        type: Sequelize.STRING
+    },
+    sobrenome:{
+        type:Sequelize.STRING
+    },
+    DataDeNascimento:{
+        type:Sequelize.data
+    },
+    cpf:{
+        type:Sequelize.INTEGER
+    }
+})
+postagem.sync({force:true})
+
+    
 sequelize.authenticate().then(function(){
     console.log("conectado")
 }).catch(function(erro){
